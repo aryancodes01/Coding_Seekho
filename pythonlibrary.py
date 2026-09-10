dd8475cea88a7bd7c8558765bd36e63e24c329bf
aryancodes01 <aryankumarsdrji@gmail.com>
2026-09-10 16:28:10 +0530

    done for today

---
+++ pythonlibrary.py
@@ -0,0 +1,20 @@
+"""
+A Python library is a collection of reusable code that helps us perform specific tasks easily and efficiently.
+
+| Library        | Used for                            |
+| -------------- | ----------------------------------- |
+| `math`         | Mathematical operations             |
+| `random`       | Generating random values            |
+| `datetime`     | Date and time                       |
+| `os`           | Operating-system related tasks      |
+| `NumPy`        | Numerical calculations and arrays   |
+| `Pandas`       | Data analysis and CSV/data handling |
+| `Matplotlib`   | Data visualization                  |
+| `Scikit-learn` | Machine Learning                    |
+| `TensorFlow`   | Deep Learning                       |
+| `PyTorch`      | Deep Learning                       |
+| `Requests`     | Working with HTTP/web APIs          |
+
+
+
+"""
