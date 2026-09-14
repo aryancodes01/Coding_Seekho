4a1d1b1a1f1b321df6d974b7342ffab19adb4454
aryancodes01 <aryankumarsdrji@gmail.com>
2026-09-14 16:59:58 +0530

    donecls

---
+++ PANDAS01.py
@@ -0,0 +1,21 @@
+"""
+
+
+
+
+
+
+
+"""
+import pandas as pd
+data= {"Name":["aryan","ashish"],
+       "Age":["23","33"],
+       "Marks":["90","40"]
+         
+         
+         }
+df=pd.DataFrame(data)
+print(df.loc[0])
+print(len(df))
+print(df.size)
+print(df.ndim)
